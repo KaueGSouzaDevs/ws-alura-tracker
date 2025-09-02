ae6782e004c6c200573a053e9177788fc2ff8ca2
GallegoDev <kauegallegosouza@gmail.com>
2025-09-02 10:02:29 -0300

    Feat: Criação e exibição de projetos (IProjeto.ts)

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,11 +1,63 @@
 <template>
-    <h1>Projetos</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvarProjeto">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">
+          Nome do Projeto
+        </label>
+          <input type="text" id="nomeDoProjeto" class="input" v-model="nomeDoProjeto"/>
+      </div>
+      <div class="field">
+        <button type="submit" class="button">
+          Salvar
+        </button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
+import IProjeto from '@/interfaces/IProjeto';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'ProjetosView',
+  data() {
+    return {
+      nomeDoProjeto: '',
+      projetos: [] as IProjeto[]
+    }
+  },
+  methods: {
+    salvarProjeto() {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString()
+      };
+      this.projetos.push(projeto);
+      this.nomeDoProjeto = '';
+    }
+  },
 });
 </script>
+
+<style scoped>
+.projetos {
+  padding: 1.5rem;
+}
+</style>
