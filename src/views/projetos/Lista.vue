a51d200bcd4b99b3fdda618b4ae7a89b066b7898
GallegoDev <kauegallegosouza@gmail.com>
2025-09-08 09:12:28 -0300

    Refactor: Rotas aninhadas

---
+++ src/views/projetos/Lista.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { key } from '@/store';
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'ListaProjetosView',
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos)
+    };
+  }
+});
+</script>
