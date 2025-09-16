623df298773aaf6d2d037d1e65925bdd7b8d97a6
GallegoDev <kauegallegosouza@gmail.com>
2025-09-16 09:36:51 -0300

    Refactor: Migração de methods do Tarefa para o setup usando Composition API

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -33,9 +33,13 @@ export default defineComponent({
             required: true
         }
     },
-    methods: {
-        tarefaClicada() : void {
-            this.$emit('aoTarefaClicada', this.tarefa)
+    setup(props, { emit }) {
+        const tarefaClicada = () : void => {
+            emit('aoTarefaClicada', props.tarefa)
+        }
+
+        return {
+            tarefaClicada
         }
     }
 })
